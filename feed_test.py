ef542e30ba1f8b1e52feea047533ef6de62643da
Abraham Frederick Grobler <20901062@sun.ac.za>
2020-09-04 21:20:07 +0200

    New folder structure

--- feed_test.py
+++ feed_test.py
@@ -2,25 +2,39 @@ import cv2
 import numpy as np
 import imutils
 
-vid = cv2.VideoCapture('rtsp://abrig:choppur22@192.168.8.130:88/videoMain')
-vid2 = cv2.VideoCapture(0)
+print("[INFO] This script allows you to stream live video from an IP camera using RTSP. Please provide the following:")
+
+cam_ip = input("Camera IP Address: ")
+cap_port = input("Camera Port: ")
+username = input("Username: ")
+password = input("Password: ")
+
+rtsp_string = 'rtsp://' + username + ':' + password + '@' + cam_ip + ':' + cap_port + '/videoMain'
+
+try:
+	vid = cv2.VideoCapture(rtsp_string)
+except:
+	print('[ERROR] Could not connect to IP camera. Please ensure that you enter the required information correctly')
+
 
 while(True):
 	ret, img = vid.read()
-	ret2, img2 = vid2.read()
 
+	if ret:
 
-	img = imutils.resize(img, width=900)
-	img2 = imutils.resize(img2, width = 900)
-	cv2.imshow('Foscam_01', img)
-	cv2.imshow('webcam', img2)
-	#print("[WARNING] Feed not available")
+		img = imutils.resize(img, width=900)
+		cv2.imshow('Foscam_01', img)
+		#print("[WARNING] Feed not available")
+	else:
+		break
+		print('[ERROR] Stream not available')
 
 	key = cv2.waitKey(1) & 0xFF
 	# if the `q` key is pressed, break from the lop
 	if key == ord("q"):
+		print('[INFO] Stream ended.')
 		break
+		
 
 # cleanup the camera and close any open windows
-vid.stop() if args.get("video", None) is None else vid.release()
 cv2.destroyAllWindows()
