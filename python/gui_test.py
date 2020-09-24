64cc3bf78f30d920f64da4b7b5f8e84adaad99eb
Abraham Frederick Grobler <20901062@sun.ac.za>
2020-09-24 13:51:14 +0200

    Can now add cameras, and view stream of new camera

--- python/gui_test.py
+++ python/gui_test.py
@@ -79,6 +79,77 @@ def update_stream(window, source):
 	default_stream  = Stream(src = source, camera_brand = 'Foscam')
 	window.destroy()
 
+def destroy_window(window):
+	window.destroy()
+
+def test_stream(label, src):
+	t_stream = Stream(src = src)
+	ret, _ = t_stream.get_stream().read()
+	status = ''
+	if ret:
+		status = 'Connection success, a stream is available: ' + src
+	else:
+		status = 'Stream not available: ' + src
+	label.config(text = status)
+
+def save_camera(window, name, src):
+	with open(filepath + "saved_cameras.txt", "a") as cam_file:
+			cam_file.write(name + ', ' + src + '\n')
+
+	destroy_window(window)
+
+def add_camera():
+
+	add_window = tkinter.Tk()
+	add_window.title("Add a new camera")
+
+	#Labels
+
+	lbl_info = tkinter.Label(add_window, text = "Please provide the following information. All fields are required. \nPlease only add the camera once the test succeeds.")
+	lbl_user = tkinter.Label(add_window, text = "Username: ")
+	lbl_password = tkinter.Label(add_window, text = "Password: ")
+	lbl_name = tkinter.Label(add_window, text = "Name the camera: ")
+	lbl_ip = tkinter.Label(add_window, text = "IPv4 Address: ")
+	lbl_port = tkinter.Label(add_window, text = "Port: ")
+	lbl_extras = tkinter.Label(add_window, text = "Additional paths")
+
+	lbl_connection_status = tkinter.Label(add_window, text = '')
+
+	#Buttons
+
+	btn_test = tkinter.Button(add_window, text = "Test connection", command = lambda: test_stream(lbl_connection_status, str(entry_user.get()) + ':' + str(entry_password.get()) + '@' + str(entry_ip.get()) + ':' + str(entry_port.get()) + str(entry_extras.get())))
+	btn_add = tkinter.Button(add_window, text = "Add camera", command = lambda: save_camera(add_window, entry_name.get(), str(entry_user.get()) + ':' + str(entry_password.get()) + '@' + str(entry_ip.get()) + ':' + str(entry_port.get()) + str(entry_extras.get())))
+
+	#Input fields
+
+	entry_user = tkinter.Entry(add_window, bd = 2)
+	entry_password = tkinter.Entry(add_window, bd = 2)
+	entry_name = tkinter.Entry(add_window, bd = 2)
+	entry_ip = tkinter.Entry(add_window, bd = 2)
+	entry_port = tkinter.Entry(add_window, bd = 2)
+	entry_extras = tkinter.Entry(add_window, bd = 2)
+
+	#Layout
+
+	lbl_info.grid( row = 0, columnspan = 2, sticky = "N")
+	lbl_name.grid(row = 1, column = 0)
+	lbl_user.grid(row = 2, column = 0)
+	lbl_password.grid(row = 3, column = 0)
+	lbl_ip.grid(row = 4, column = 0)
+	lbl_port.grid(row = 5, column = 0)
+	lbl_extras.grid(row = 6, column = 0)
+	entry_name.grid(row = 1, column = 1)
+	entry_user.grid(row = 2, column = 1)
+	entry_password.grid(row = 3, column = 1)
+	entry_ip.grid(row = 4, column = 1)
+	entry_port.grid(row = 5, column = 1)
+	entry_extras.grid(row = 6, column = 1)
+	btn_test.grid(row = 7, column = 0)
+	lbl_connection_status.grid(row = 7, column = 1)
+	btn_add.grid(row = 8, column = 0)
+
+
+
 
 # Window elements
 
@@ -88,7 +159,7 @@ lbl_default = tkinter.Label(window, text = "Select a camera to view stream")
 
 # Buttons
 btn_select = tkinter.Button(window, text = "Select a stream", command = show_cameras)
-btn_add = tkinter.Button(window, text = "Add a camera")
+btn_add = tkinter.Button(window, text = "Add a camera", command = add_camera)
 btn_detele = tkinter.Button(window, text = "Delete a camera")
 btn_calibrate = tkinter.Button(window, text = "Calibrate system")
 btn_stream = tkinter.Button(window, text = "Show stream", command = show_frame)
@@ -97,13 +168,13 @@ video_stream = tkinter.Label(window)
 
 #Layout
 btn_select.grid(row = 0, column = 0)
-btn_stream.grid(row=1, column = 0)
-btn_add.grid(row = 0, column = 1)
-btn_detele.grid(row = 0, column = 2)
-btn_calibrate.grid(row = 0, column = 3)
-lbl_calibration_status.grid(row = 2, column = 0)
-lbl_default.grid(row = 3, column = 0)
-video_stream.grid(row = 4, column = 0)
+btn_stream.grid(row=0, column = 1)
+btn_add.grid(row = 0, column = 2)
+btn_detele.grid(row = 0, column = 3)
+btn_calibrate.grid(row = 0, column = 4)
+lbl_calibration_status.grid(row = 2, columnspan = 2, sticky = 'W')
+lbl_default.grid(row = 3, columnspan = 2, sticky = 'W')
+video_stream.grid(columnspan = 5, sticky = 'W')
 
 tkinter.mainloop()
 
--- python/stream.py
+++ python/stream.py
@@ -3,20 +3,19 @@ import PIL
 from PIL import Image, ImageTk
 
 class Stream:
+	cap = cv2.VideoCapture()
 
-	def __init__(self, src='0', camera_brand=''):
+	def __init__(self, src='0'):
+		global cap
 		self.src = src
 		self.camera_brand = camera_brand
 		self.streaming = False
 
-		if src == '0':
+		if src == '0' or src == ':@0:': #Enable webcam support
 			self.cap = cv2.VideoCapture(0)
 		else:
-			if camera_brand == 'Foscam':
-				rtsp_string = 'rtsp://' + src + '/videoMain'
-				self.cap = cv2.VideoCapture(rtsp_string)
-			else:
-				print("[ERROR] Please provide a brand")
+			rtsp_string = 'rtsp://' + src + '/videoMain'
+			self.cap = cv2.VideoCapture(rtsp_string)
 
 	def get_stream(self):
 		#print("Source:", self.src)
